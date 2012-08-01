5c28b6858028889bba658fbc5e23eb589bf889b8
Yohan Boniface <yb@enix.org>
2012-08-01 22:49:12 +0200

    Change admin path

--- carte_fluviale/urls.py
+++ carte_fluviale/urls.py
@@ -28,6 +28,6 @@ urlpatterns += patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^poupe/', include(admin.site.urls)),
 )
 urlpatterns += staticfiles_urlpatterns()
