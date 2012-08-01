a3f794d95fa3052af7ca3ce46e46c99b43fd8383
Yohan Boniface <yb@enix.org>
2012-08-01 02:05:32 +0200

    Fix dev settings path

--- carte_fluviale/wsgi/dev.py
+++ carte_fluviale/wsgi/dev.py
@@ -15,7 +15,7 @@ framework.
 """
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "carte_fluviale.conf.dev.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "carte_fluviale.conf.dev")
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
