b853d287b2309ffb46d536bbe70565ac96370687
Yohan Boniface <yb@enix.org>
2012-08-08 19:27:37 +0200

    Make the map editable for anonymous too, for now

--- carte_fluviale/urls.py
+++ carte_fluviale/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.contrib.auth.decorators import permission_required
+# from django.contrib.auth.decorators import permission_required
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -9,14 +9,14 @@ admin.autodiscover()
 from chickpea.urls import urlpatterns
 from chickpea.views import MapView
 
-urls_to_decorate = {
-    "marker_update": permission_required('marker.can_change'),
-    "marker_add": permission_required('marker.can_add')
-}
+# urls_to_decorate = {
+#     "marker_update": permission_required('marker.can_change'),
+#     "marker_add": permission_required('marker.can_add')
+# }
 
-for urlpattern in urlpatterns:
-    if urlpattern.name in urls_to_decorate:
-        urlpattern._callback = urls_to_decorate[urlpattern.name](urlpattern.callback)
+# for urlpattern in urlpatterns:
+#     if urlpattern.name in urls_to_decorate:
+#         urlpattern._callback = urls_to_decorate[urlpattern.name](urlpattern.callback)
 
 urlpatterns += patterns('',
     url(r'^$', MapView.as_view(), {"slug": "fluvial"}, name='fluvial_map'),
